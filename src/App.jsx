03f5c9e1bba32c996d73d8208f44e6b0cb66cf77
Kauãn Santos <165726707+wikipay007@users.noreply.github.com>
2025-08-20 21:29:30 -0300

    Add Pages and components
    Adicionado novas páginas;
    Criação de lógica para navegação (app.jsx)
    Criação de componentes (Cards, links)
    Alterado a logo no header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+<<<<<<< Updated upstream
 import Header from './components/Header';
 import { useState } from 'react';
+=======
+import Geral from "./pages/Geral.jsx";
+import Turmas from "./pages/turmas.jsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
+<<<<<<< Updated upstream
       <Header />
+=======
+     <Router>
+      <Routes>
+        {/*<Route path="/" element={<Home />} />*/}
+        {/*<Route path="/login" element={<Login />} />*/}
+        <Route path='/turmas' element={<Turmas/>} />
+        <Route path='/geral' element={<Geral/>} />
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
       
     </>
   );
