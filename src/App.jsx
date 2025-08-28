9b363f60065a1cd6a0c40ea90ec92b90a293b691
Kauãn Santos <165726707+wikipay007@users.noreply.github.com>
2025-08-28 20:59:57 -0300

    Alterado a rota para novas páginas
    Adicionado as novas rotas.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login.jsx";
 import Turmas from "./pages/Turmas.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AtividadePage from "./pages/AtividadePage.jsx";
+import FavoritosPage from "./pages/FavoritosPage.jsx";
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
         <Route path='/turmas' element={<Turmas/>} />
         <Route path='/geral' element={<Geral/>} />
         <Route path='/cadastro' element={<Cadastro/>} />
+        <Route path='/atividades' element={<AtividadePage/>} />
+        <Route path='/favoritos' element={<FavoritosPage/>} />
       </Routes>
     </Router>    
     </>
