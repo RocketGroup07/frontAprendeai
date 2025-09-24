8374146fe282ec08a85a5d645c173a24e180b29b
Jotave313 <joaovbs.jvbs@gmail.com>
2025-09-24 19:15:31 -0300

    tela turmas update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-
-
+import MenuDropdown from './MenuDropdown';
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className="flex bg-[#D00909] justify-between p-6 items-center">
-
       <div className="object-contain w-54 ml-12"> 
         <img src="../images/logoAP.png" />
       </div>
-
       <div className="flex items-center justify-between gap-6">
         <p className='text-white text-[25px] font-[400]'>Ciclano</p>
-
         <div className="cursor-pointer text-white">
-            <FaUserCircle size={35}/>
-            </div>
-
-        <div className="cursor-pointer text-white">
+          <FaUserCircle size={35}/>
+        </div>
+        <div
+          className="cursor-pointer text-white"
+          onClick={() => setShowMenu(!showMenu)}
+        >
           <GiHamburgerMenu size={35}/>
         </div>
-
+        {showMenu && <MenuDropdown />}
       </div>
     </header>
   );
