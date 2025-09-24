fc032a0c219af12b547cef85b029fb1cfe29e611
Jotave313 <joaovbs.jvbs@gmail.com>
2025-09-24 20:53:53 -0300

    componente menu dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import MenuDropdown from './MenuDropdown';
 import { useAuth } from './UserAuth';
- 
- 
- 
+
 function Header() {
   const auth = useAuth();
   const usuario = auth?.usuario;
-  const userName = usuario?.nome || "Usuário"
- 
+  const userName = usuario?.nome || "Usuário";
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className="flex bg-[#D00909] justify-between p-6 items-center">
- 
-      <div className="object-contain w-54 ml-12">
+      <div className="object-contain w-54 ml-12"> 
         <img src="../images/logoAP.png" />
       </div>
-
       <div className="flex items-center justify-between gap-6">
         <p className='text-white text-[25px] font-[400]'>{userName}</p>
- 
         <div className="cursor-pointer text-white">
-          <FaUserCircle size={35} />
+          <FaUserCircle size={35}/>
         </div>
- 
-        <div className="cursor-pointer text-white">
-          <GiHamburgerMenu size={35} />
+        <div
+          className="cursor-pointer text-white"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <GiHamburgerMenu size={35}/>
         </div>
+        {showMenu && <MenuDropdown />}
       </div>
     </header>
   );
 }
- 
+
 export default Header;
