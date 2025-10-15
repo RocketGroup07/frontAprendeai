7f622f6f7f81be7d37b96d57fe4091fc4d60e2cf
Kayque Cassiano <kayque.cassiano1@gmail.com>
2025-10-14 21:19:29 -0300

    feat(LinkRedirecionavel, LinksContainer)
    adição da lógica para que o fundo do botão mude, de acordo se o botão esta selecionado ou não

--- src/components/LinksContainer.jsx
+++ src/components/LinksContainer.jsx
@@ -1,15 +1,27 @@
-import LinkRedirecionavel from './LinkRedirecionavel'
+import LinkRedirecionavel from "./LinkRedirecionavel";
 
 function LinksContainer({ turmaId, children }) {
   return (
-    <div className='w-[90%] mr-auto ml-auto mt-4 flex flex-row gap-[48px] p-1 text-[var(--text)]'>
-      <LinkRedirecionavel nome={"Geral"} link={`/geral/${turmaId}`} className="bg-[var(--primary)] text-[var(--text)] p-2 rounded cursor-pointer" />
-      <LinkRedirecionavel nome={"Atividades"} link={`/atividades/${turmaId}`} className="p-2 cursor-pointer" />
-      <LinkRedirecionavel nome={"Favoritos"} link={`/favoritos/${turmaId}`} className="p-2 cursor-pointer" />
+    <div className="w-[90%] mr-auto ml-auto mt-4 flex flex-row gap-[48px] p-1 text-[var(--text)]">
+      <LinkRedirecionavel
+        nome="Geral"
+        link={`/geral/${turmaId}`}
+        className="p-2 rounded cursor-pointer"
+      />
+      <LinkRedirecionavel
+        nome="Atividades"
+        link={`/atividades/${turmaId}`}
+        className="p-2 rounded cursor-pointer"
+      />
+      <LinkRedirecionavel
+        nome="Favoritos"
+        link={`/favoritos/${turmaId}`}
+        className="p-2 rounded cursor-pointer"
+      />
 
       {children}
     </div>
-  )
+  );
 }
 
-export default LinksContainer
+export default LinksContainer;
