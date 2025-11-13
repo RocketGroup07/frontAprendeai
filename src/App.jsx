c645c99c2f0546851763d5b7555631fa6f43d3f4
Kauãn Santos <165726707+wikipay007@users.noreply.github.com>
2025-11-12 21:30:14 -0300

    Feat - Copiar código da turma e algumas outras
    Agora é possível copiar o código da turma

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from "./components/UserAuth.jsx";
 import DashProf from "./pages/DashProf.jsx";
 import TelaPost from "./pages/TelaPost.jsx";
-import TelaAtividade from "./pages/TelaAtividade.jsx";
 
 function App() {
 
@@ -27,7 +26,6 @@ function App() {
             <Route path='/favoritos/:turmaId' element={<FavoritosPage />} />
             <Route path='/professor' element={<DashProf />} />
             <Route path='/post/:turmaId/:postId' element={<TelaPost />} />
-            <Route path='/atividades/:turmaId/:atividadeId' element={<TelaAtividade/>} />
           </Routes>
         </Router>
       
