c76a2f912d7bba7910e76360d88a1201fdbd3171
João Victor <joaovbs.jvbs@gmail.com>
2025-11-14 18:05:14 -0300

    Revert "Bug: morreu"
    This reverts commit 1633ea166b0c0436c73deac7183b4c811c08bd2a.

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export const api = axios.create({
 
-   
    baseURL: "http://localhost:8080/",
- 
+   headers: {
+      "Content-Type": "application/json",
+   }
 });
 
 const requestInterceptor = (config) => {
